35686407b4f3fb9b099b5c5233cd1ca8cb6e79e2
Tom Pinataro <tompinataro@gmail.com>
2026-01-19 11:00:10 -0500

    Clean up unused legacy materials

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-
-import store from './redux/store';
-
-import App from './components/App/App';
+import App from '../shared/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
