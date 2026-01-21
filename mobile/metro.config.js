513f46ba7048b8470c32c603314bc737f239e76b
Tom Pinataro <tompinataro@gmail.com>
2026-01-20 20:43:56 -0500

    fix: allow metro to resolve workspace deps

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,8 +2,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
 const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..');
 const config = getDefaultConfig(projectRoot);
-config.watchFolders = [path.resolve(projectRoot, 'src', 'shared')];
-config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
+config.watchFolders = [
+  path.resolve(projectRoot, 'src', 'shared'),
+  workspaceRoot,
+];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
